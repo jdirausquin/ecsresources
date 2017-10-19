25305a9e440b8893001123f64e122b0cf84dd0f2
JD Irausquin <jdirausquin@gmail.com>
2017-10-19 13:17:47 -0600

    Changing config

--- deploy.tf
+++ deploy.tf
@@ -4,12 +4,3 @@ resource "aws_ecs_task_definition" "taskdef" {
     family                = "taskdefname"
     container_definitions = "${file("containerdefinition.json")}"
 }
-
-resource "aws_cloudwatch_log_group" "yada" {
-  name = "Yada"
-}
-
-resource "aws_cloudwatch_log_stream" "foo" {
-  name           = "SampleLogStream1234"
-  log_group_name = "${aws_cloudwatch_log_group.yada.name}"
-}
